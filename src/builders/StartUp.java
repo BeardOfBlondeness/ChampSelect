a96d6a32acd2d4b32d35a458b5f3aa4cbae7efb9
Pear <mpearcey.59024@farnborough.ac.uk>
2017-12-16 16:52:02 +0000

    Added the actual code we will use i think

--- src/builders/StartUp.java
+++ src/builders/StartUp.java
@@ -3,34 +3,44 @@ package builders;
 import net.rithms.riot.api.ApiConfig;
 import net.rithms.riot.api.RiotApi;
 import net.rithms.riot.api.RiotApiException;
+import net.rithms.riot.api.endpoints.match.dto.Match;
 import net.rithms.riot.api.endpoints.match.dto.MatchList;
 import net.rithms.riot.api.endpoints.match.dto.MatchReference;
 import net.rithms.riot.api.endpoints.summoner.dto.Summoner;
 import net.rithms.riot.constant.Platform;
 
 public class StartUp {
-
+	static RiotApi api;
+	static ApiConfig config;
 	public static void main(String[] args) throws RiotApiException {
-		ApiConfig config = new ApiConfig().setKey("RGAPI-f81821ac-6225-4ef6-b2f6-c43bb1b27f2b");
-		RiotApi api = new RiotApi(config);
+		config = new ApiConfig().setKey("RGAPI-27ff443f-2c2f-4ba2-b03c-1f9adeb00a32");
+		api = new RiotApi(config);
 
 		// First we need to request the summoner because we will need it's account ID
 		Summoner summoner = api.getSummonerByName(Platform.EUW, "Ghaster");
 
 		// Then we can use the account ID to request the summoner's match list
 		MatchList matchList = api.getMatchListByAccountId(Platform.EUW, summoner.getAccountId());
-
+		
+		
 		System.out.println("Total Games in requested match list: " + matchList.getTotalGames());
 
 		// We can now iterate over the match list to access the data
 		if (matchList.getMatches() != null) {
 			for (MatchReference match : matchList.getMatches()) {
-				System.out.println("GameID: " + match.getGameId());
-				System.out.println(match.getChampion() + " was the champ they played");
-				System.out.println(match.getRole() + " was the role they played");
-				System.out.println(match.getLane() + " was the lane they played");
-				System.out.println(match.toString() + "\n\n\n");
+				Match mat = api.getMatch(Platform.EUW, match.getGameId());
+				System.out.println(getOtherChampions(mat.getGameId(), summoner.getName(), summoner.getAccountId(), match));
+				
 			}
 		}
+	}
+	
+	public static String getOtherChampions(Long matchID, String summonerID, Long accountID, MatchReference match) throws RiotApiException {
+		System.out.println("Ghaster played " + match.getChampion());
+		Match tempMatch = api.getMatch(Platform.EUW, matchID);
+		for(int i = 0; i < 4; i++) {
+			
+		}
+		return "";		
 	}
 }
