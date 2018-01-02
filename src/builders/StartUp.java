b405610e72916ce733330addc4077b25d2b59505
Pear <mpearcey.59024@farnborough.ac.uk>
2018-01-02 22:05:17 +0000

    Added calculation for which team the player is on

--- src/builders/StartUp.java
+++ src/builders/StartUp.java
@@ -21,16 +21,17 @@ public class StartUp {
 	static ApiConfig config;
 	static Account acc;
 	public static ChampionStore CS;
+	static int teamPosition;
 	
 	public static void main(String[] args) throws RiotApiException, InterruptedException {
 		loadPreferences();
 		config = new ApiConfig().setKey(StartUp.apiKey);
 		api = new RiotApi(config);
 		CS = new ChampionStore(api);
-		acc = new Account(api.getSummonerByName(Platform.EUW, "Malagith").getId());
+		acc = new Account(api.getSummonerByName(Platform.EUW, "Ghaster").getId());
 		
 		// First we need to request the summoner because we will need it's account ID
-		Summoner summoner = api.getSummonerByName(Platform.EUW, "Malagith");
+		Summoner summoner = api.getSummonerByName(Platform.EUW, "Ghaster");
 
 		// Then we can use the account ID to request the summoner's match list
 		MatchList matchList = api.getMatchListByAccountId(Platform.EUW, summoner.getAccountId());
@@ -41,7 +42,7 @@ public class StartUp {
 		if (matchList.getMatches() != null) {
 			for (MatchReference match : matchList.getMatches()) {
 				Match mat = api.getMatch(Platform.EUW, match.getGameId());
-				Thread.sleep(500);
+				Thread.sleep(1000);
 				System.out.println(getOtherChampions(mat.getGameId(), summoner.getName(), summoner.getAccountId(), match));
 			}
 		}
@@ -62,16 +63,25 @@ public class StartUp {
 	}
 	
 	public static String getOtherChampions(Long matchID, String summonerID, Long accountID, MatchReference match) throws RiotApiException {
-		
+		teamPosition = -1;
 		System.out.println("Ghaster played " + match.getChampion());
 		Match tempMatch = api.getMatch(Platform.EUW, matchID);
 		List participants = tempMatch.getParticipantIdentities();
 		
+		/*
+		 * Initial Loop to calculate which player the account of interest is
+		 */
 		for(int i = 1; i < 11; i++) {
 			System.out.println(CS.get(api.getMatch(Platform.EUW, matchID).getParticipantByParticipantId(i).getChampionId()));
-			System.out.println(api.getMatch(Platform.EUW, matchID).getParticipantByParticipantId(i).getStats().isWin());
-			System.out.println(api.getMatch(Platform.EUW, matchID).getParticipantByParticipantId(i).getTeamId() + " is ghasters team");
+			//System.out.println(api.getMatch(Platform.EUW, matchID).getParticipantByParticipantId(i).getStats().isWin());
+			//System.out.println(api.getMatch(Platform.EUW, matchID).getParticipantByParticipantId(i).getTeamId() + " is ghasters team");
+			
+			if(tempMatch.getParticipantByParticipantId(i).equals(tempMatch.getParticipantByAccountId(accountID))) {
+				teamPosition = i;
+				break;
+			}
 		}
+		System.out.println(teamPosition);
 		return "\n";		
 	}
 }
